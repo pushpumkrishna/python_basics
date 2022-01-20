9158d276a70ba9a153d779b930a585744a87c731
pushpumkrishna <Greedyseeker>
2022-01-20 18:39:51 +0530

    updated gitignore

--- codes/item_grid.py
+++ codes/item_grid.py
@@ -1,29 +1,25 @@
 #!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
+from typing import Any
 
 row = 4
 column = 3
-string = "| aaa | aaa | aaa |"
+test_string = "| aaa | aaa | aaa |"
 
 print("-" * 20)
-def f(row, column, string):
-    for i in range( 1, row + 1):
-        for i in range(1, column + 1):
-            if (i == 0 or i == 5):
-                print(" ", end =" ")
+
+
+def grid_maker(rows: int,
+               columns: int,
+               string: str) -> Any:
+    for i in range(1, rows + 1):
+        for j in range(1, columns + 1):
+            if i == 0 or j == 5:
+                print(" ", end=" ")
             else:
                 print("| aaa | aaa | aaa |")
                 if i == 3 or i == 6 or i == 10:
                     print("-" * 20)
-            
-f(row, column, string)
 
 
-# In[ ]:
-
-
-
-
+# uncomment below to run
+# grid_maker(row, column, test_string)
